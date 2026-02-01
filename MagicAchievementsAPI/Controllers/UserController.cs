206fd8f9e2d1bdaed43345bd1b0e960a9638f40b
Jannis <jannishermeling@web.de>
2026-02-01 14:08:02 +0100

    Added UserController in API project

--- MagicAchievementsAPI/Controllers/UserController.cs
+++ MagicAchievementsAPI/Controllers/UserController.cs
@@ -6,12 +6,12 @@ namespace MagicAchievementsAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MagicAchievementsController : ControllerBase
+    public class UserController : ControllerBase
     {
 
         private readonly IWebHostEnvironment _env;
 
-        public MagicAchievementsController(IWebHostEnvironment env)
+        public UserController(IWebHostEnvironment env)
         {
             _env = env;
         }
@@ -22,29 +22,33 @@ namespace MagicAchievementsAPI.Controllers
         {
             // C:\Users\janni\source\repos\MagicAchievements\MagicAchievementsAPI
             var path = Path.Combine(_env.WebRootPath, "data.txt");
-            if (!System.IO.File.Exists(path)) {
+            if (!System.IO.File.Exists(path))
+            {
                 System.IO.File.WriteAllText(path, "Testuser,1.1.1,");
             }
             return System.IO.File.ReadAllText(path);
         }
 
-
-        // GET api/<MagicAchievementsController>/5
+        // GET api/<UserController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<MagicAchievementsController>
+        // POST api/<UserController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
-
         }
 
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
 
-        // DELETE api/<MagicAchievementsController>/5
+        // DELETE api/<UserController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
